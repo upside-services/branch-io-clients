c3216dcd06c39021546bc6e1afeb0a496638739b
Thomas Vaughan <tom@upside-services.com>
2016-11-22 13:35:44 -0500

    Adding a retrying functionality to the client package
    Also changing the interface from URL to URI to be a little more
    forgiving when doing .equals comparisons and a little more future proof

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,10 @@
+# Branch.io Java Client
+
+## 0.2.0
+
+* Changed interface to return URIs instead of URLs
+* Added support for wrapping BranchClient implementations in a backoff & retry facade
+
+## 0.1.0
+
+* Initial release
--- src/test/java/com/upside/branch_io/client/TestBranchClientFactory.java
+++ src/test/java/com/upside/branch_io/client/TestBranchClientFactory.java
@@ -17,11 +17,30 @@ public class TestBranchClientFactory {
         assertTrue("Expected a real client", factory.build() instanceof RetrofitBranchClient);
     }
 
+
+    @Test
+    public void testRetryingClientCreation() {
+        BranchClientFactory factory = new BranchClientFactory();
+        factory.setClientType("REAL");
+        factory.setApiKey("foo");
+        factory.setApiSecret("secret");
+        factory.setRetryAttempts(1);
+        assertTrue("Expected a real retrying client", factory.build() instanceof RetryingBranchClient);
+    }
+
     @Test
     public void testMockClientCreation() {
         BranchClientFactory factory = new BranchClientFactory();
         factory.setClientType("MOCK");
         assertTrue("Expected a mock client", factory.build() instanceof MockBranchClient);
+    }
+
+    @Test
+    public void testRetryingMockClientCreation() {
+        BranchClientFactory factory = new BranchClientFactory();
+        factory.setClientType("MOCK");
+        factory.setRetryAttempts(2);
+        assertTrue("Expected a mock client", factory.build() instanceof RetryingBranchClient);
     }
 
     @Test

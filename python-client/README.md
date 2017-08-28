9e0a49446d27e0a03560cd60d1170e0737dc92a1
Thomas Vaughan <tom@upside-services.com>
2017-08-28 10:03:56 +0200

    making this in to a release client python package

--- python-client/README.md
+++ python-client/README.md
@@ -1,2 +1,17 @@
-# fix_branch_io_links
-Fixes bad dataMaps in our branch.io referral code links
+# Branch.io client
+
+The client is invoked like:
+
+```
+from upside_core.tiers import load_configuration
+from branch_io.client import BranchClient
+
+config = load_configuration('prod', key='branch_io.json', bucket='credentials.upside-services.com')
+
+branch_client = BranchClient(config['key'], config['secret'], config['url'])
+
+# Get remote branch.io information about our "uberdc" referral code
+branch_client.get_deep_link('uberdc')
+
+```
+
--- python-client/setup.py
+++ python-client/setup.py
@@ -1,12 +1,21 @@
-from distutils.core import setup
+#!/usr/bin/env python
 
-setup(
-    name='client',
-    version='1.0.0',
-    packages=['client'],
-    url='',
-    license='',
-    author='tvaughan',
-    author_email='',
-    description=''
+from setuptools import setup, find_packages
+
+import upside_core
+
+requires = ['boto3', 'requests']
+
+setup_options = dict(
+    name='branch-client',
+    version='1.0.1',
+    description='Python client for branch.io.',
+    long_description='Python client for branch.io.',
+    author='Upside Services, Inc',
+    url='https://github.com/upside-services/branch-io-clients',
+    scripts=[],
+    packages=find_packages(exclude=['tests*']),
+    install_requires=requires
 )
+
+setup(**setup_options)
